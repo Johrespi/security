fef66bb8148441dc44e65f9932701bd2790f7836
Johrespi <139030531+Johrespi@users.noreply.github.com>
2024-07-22 09:39:31 -0500

    GUIDE 19 STOP 2

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+ /* 1. Importe el módulo crypto */
+ let crypto = require('crypto');
+
+
  /* 1. Cargue los modelos de acuerdo con la configuración de la conexión */
  const sequelize = require('../models/index.js').sequelize;
  var initModels = require("../models/init-models");
